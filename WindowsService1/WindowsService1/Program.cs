f7dcbf7a11afeebdc17fc7adb17afc18cc2e60ed
Navraj Khanal <navrajkhanal2001@gmail.com>
2026-01-20 08:53:32 +0530

    Testing updates
    Testing Failed 20th Jan - need updates

--- WindowsService1/WindowsService1/Program.cs
+++ WindowsService1/WindowsService1/Program.cs
@@ -4,6 +4,22 @@ using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+;
+using Microsoft.Extensions.Hosting.WindowsServices;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+// This tells the app to behave like a service when installed, 
+// but it stays a console app when you press F5 in Visual Studio.
+builder.Services.AddWindowsService(options =>
+{
+    options.ServiceName = "Satark";
+});
+
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
 
 namespace WindowsService1
 {
